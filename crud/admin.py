9c219d777a6c96bb547f312ed00e28f0333b219c
mrdybsn <mardybesana0705@gmail.com>
2025-06-13 16:53:43 +0800

    Tindahan 404

--- crud/admin.py
+++ crud/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django import forms
+from .models import (User, Category, Product, Sale, SaleItem, Supplier, Purchase, PurchaseItem)
 
-# Register your models here.
+class UserAdminForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'border rounded p-2 w-full'}),
+            'password': forms.PasswordInput(attrs={'class': 'border rounded p-2 w-full'}),
+            'role': forms.Select(attrs={'class': 'border rounded p-2 w-full'}),
+            'contact_number': forms.TextInput(attrs={'class': 'border rounded p-2 w-full'}),
+            'address': forms.Textarea(attrs={'class': 'border rounded p-2 w-full'}),
+            'gender': forms.Select(attrs={'class': 'border rounded p-2 w-full'}),
+            'birth_date': forms.DateInput(attrs={'class': 'border rounded p-2 w-full', 'type': 'date'}),
+        }
+
+class UserAdmin(admin.ModelAdmin):
+    form = UserAdminForm
+    list_display = ('username', 'role', 'contact_number', 'email')
+    search_fields = ('username', 'email', 'contact_number')
+    list_filter = ('role', 'gender')
+    ordering = ('username',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Sale)
+admin.site.register(SaleItem)
+admin.site.register(Supplier)
+admin.site.register(Purchase)
+admin.site.register(PurchaseItem)
+admin.site.site_header = "Tindahan404"
+admin.site.site_title = "Admin Panel"
+admin.site.index_title = "Welcome Back, Admin!"
